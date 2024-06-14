fdc72a511b5afb1c716a805a677cb63b3753a984
Admin <drashtipatel7080@gmail.com>
2024-06-14 18:12:39 +0530

    14-06-'24

--- lib/screens/home/components/app_bar.dart
+++ lib/screens/home/components/app_bar.dart
@@ -2,27 +2,6 @@ import 'package:flutter/material.dart';
 
 SliverAppBar sliverAppbar() {
   return SliverAppBar(
-    // title: const Column(
-    //   crossAxisAlignment: CrossAxisAlignment.start,
-    //   children: [
-    //     Text(
-    //       'Hello Everyone',
-    //       style: TextStyle(
-    //           color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
-    //     ),
-    //     Text(
-    //       'Good Morning 👋',
-    //       style: TextStyle(
-    //           color: Colors.white, fontWeight: FontWeight.bold, fontSize: 12),
-    //     ),
-    //   ],
-    // ),
-    // leading: const Padding(
-    //   padding: EdgeInsets.all(5),
-    //   child: CircleAvatar(
-    //     backgroundImage: AssetImage('assets/img/dp.jpeg'),
-    //   ),
-    // ),
     backgroundColor: Colors.transparent,
     expandedHeight: 200,
     flexibleSpace: FlexibleSpaceBar(
